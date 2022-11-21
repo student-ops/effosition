54727f707751df4aeb3f32cfcb4283064f108468
student-ops <gogakuec@gmail.com>
2022-11-21 13:09:08 +0900

    a

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(www.croud-crab.com  croud-crab)
+domains=(www.croud-crab.com  croud-crab.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="gogakuec@gmail.com" # Adding a valid address is strongly recommended
